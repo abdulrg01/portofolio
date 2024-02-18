c40a12af80d4d0ea726b4345cbc2c5c43be72026
abdulrg01 <abdulrg01@gmail.com>
2024-02-18 11:53:51 +0100

    UI

--- client/pages/components/Experience.jsx
+++ client/pages/components/Experience.jsx
@@ -5,7 +5,7 @@ import { useGetSkillsDataQuery } from "@/redux/skills/skillsApiSlice";
 export default function Experience() {
   const [history, setHistory] = useState([]);
   const [skills, setSkills] = useState([]);
-  const { data: historyData, isLoading } = useGetHistoryDataQuery(
+  const { data: historyData } = useGetHistoryDataQuery(
     {},
     {
       refetchOnMountOrArgChange: true,
@@ -20,75 +20,69 @@ export default function Experience() {
 
   useEffect(() => {
     if (historyData) {
-      setHistory(data?.history);
+      setHistory(data.history);
     }
   }, [historyData]);
 
   useEffect(() => {
     if (data) {
-      setSkills(data?.skills);
+      setSkills(data.skills);
     }
   }, [data]);
 
   return (
-    <>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <section id="experiance" className="bg-gray-200 dark:bg-[#020A13]">
-          <div className="max-w-[1000px] mx-auto py-5 lg:py-14">
-            <h2 class="mb-20 head_text blue_gradient title">Experience</h2>
-            <div className="content flex flex-row justify-evenly mt-[14px] ">
-              <div className="skills w-[45%] flex flex-wrap gap-[37px]">
-                {skills &&
-                  skills.map((skill, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="skill flex flex-col items-center gap-3"
-                      >
-                        <div className="skillImg rounded-full flex items-center justify-center w-[120px] h-[120px]">
-                          <img
-                            className="w-[75px]"
-                            src={skill.img.url}
-                            alt={skill.title}
-                          />
-                        </div>
-                        <p className="text-[20px] font-[500] text-black dark:text-gray-300">
-                          {skill.title}
-                        </p>
-                      </div>
-                    );
-                  })}
-              </div>
-              <ul className="history w-[45%] flex flex-col gap-[30px]">
-                {history &&
-                  history.map((item, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className="historyItem flex flex-row items-center gap-[10px] bg-[#19376d]"
-                      >
-                        <img
-                          className="w-[50px]"
-                          src={item.imageSrc.url}
-                          alt={item.organisation}
-                        />
-                        <div className="historyItemDetails">
-                          <h3 className="text-[20px] font-[400] text-gray-300">
-                            {item.organisation}
-                          </h3>
-                          <p className="desc">{item.experiences}</p>
-                          <p className="desc">{`${item.startDate} ${item.endDate}`}</p>
-                        </div>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
+    <section id="experiance" className="bg-gray-200 dark:bg-[#020A13]">
+      <div className="max-w-[1000px] mx-auto py-5 lg:py-14">
+        <h2 class="mb-20 head_text blue_gradient title">Experience</h2>
+        <div className="content flex flex-row justify-evenly mt-[14px] ">
+          <div className="skills w-[45%] flex flex-wrap gap-[37px]">
+            {skills &&
+              skills.map((skill, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="skill flex flex-col items-center gap-3"
+                  >
+                    <div className="skillImg rounded-full flex items-center justify-center w-[120px] h-[120px]">
+                      <img
+                        className="w-[75px]"
+                        src={skill.img.url}
+                        alt={skill.title}
+                      />
+                    </div>
+                    <p className="text-[20px] font-[500] text-black dark:text-gray-300">
+                      {skill.title}
+                    </p>
+                  </div>
+                );
+              })}
           </div>
-        </section>
-      )}
-    </>
+          <ul className="history w-[45%] flex flex-col gap-[30px]">
+            {history &&
+              history.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="historyItem flex flex-row items-center gap-[10px] bg-[#19376d]"
+                  >
+                    <img
+                      className="w-[50px]"
+                      src={item.imageSrc.url}
+                      alt={item.organisation}
+                    />
+                    <div className="historyItemDetails">
+                      <h3 className="text-[20px] font-[400] text-gray-300">
+                        {item.organisation}
+                      </h3>
+                      <p className="desc">{item.experiences}</p>
+                      <p className="desc">{`${item.startDate} ${item.endDate}`}</p>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 }
--- client/pages/components/ProjectSection.jsx
+++ client/pages/components/ProjectSection.jsx
@@ -12,7 +12,7 @@ export default function ProjectSection() {
     animate: { y: 0, opacity: 1 },
   };
 
-  const { data, isLoading } = useGetProjectsDataQuery(
+  const { data } = useGetProjectsDataQuery(
     {},
     {
       refetchOnMountOrArgChange: true,
@@ -21,51 +21,45 @@ export default function ProjectSection() {
 
   useEffect(() => {
     if (data) {
-      setProjects(data?.projects);
+      setProjects(data.projects);
     }
   }, [data]);
 
   return (
-    <>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="bg-gray-200 dark:bg-[#020A13]" id="project">
-          <div className="max-w-[1000px] mx-auto py-5 lg:py-14">
-            <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-              <h2 class="mb-4 head_text blue_gradient">My Projects</h2>
-              <p class="font-light text-gray-700 lg:mb-16 sm:text-xl">
-                Explore the whole collection of open-source web components and
-                elements built with the utility classes from Tailwind
-              </p>
-            </div>
-            <ul
-              ref={ref}
-              class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-2 grid-cols-2"
-            >
-              {projects &&
-                projects.map((item, index) => (
-                  <motion.li
-                    key={index}
-                    variants={cardVariants}
-                    initial="initial"
-                    animate={isInView ? "animate" : "initial"}
-                    transition={{ duration: 0.3, delay: index + 0.4 }}
-                  >
-                    <ProjectCard
-                      key={index}
-                      title={item.title}
-                      desc={item.desc}
-                      img={item.imageSrc.url}
-                      gitUrl={item?.gitUrl}
-                      previewUrl={item?.previewUrl}
-                    />
-                  </motion.li>
-                ))}
-            </ul>
-          </div>
+    <div className="bg-gray-200 dark:bg-[#020A13]" id="project">
+      <div className="max-w-[1000px] mx-auto py-5 lg:py-14">
+        <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+          <h2 class="mb-4 head_text blue_gradient">My Projects</h2>
+          <p class="font-light text-gray-700 lg:mb-16 sm:text-xl">
+            Explore the whole collection of open-source web components and
+            elements built with the utility classes from Tailwind
+          </p>
         </div>
-      )}
-    </>
+        <ul
+          ref={ref}
+          class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-2 grid-cols-2"
+        >
+          {projects &&
+            projects.map((item, index) => (
+              <motion.li
+                key={index}
+                variants={cardVariants}
+                initial="initial"
+                animate={isInView ? "animate" : "initial"}
+                transition={{ duration: 0.3, delay: index + 0.4 }}
+              >
+                <ProjectCard
+                  key={index}
+                  title={item.title}
+                  desc={item.desc}
+                  img={item.imageSrc.url}
+                  gitUrl={item?.gitUrl}
+                  previewUrl={item?.previewUrl}
+                />
+              </motion.li>
+            ))}
+        </ul>
+      </div>
+    </div>
   );
 }
